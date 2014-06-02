bb42cc3b1fb0f39824d1c08a63d9582d204d7b60
Joel Cross <joel@kazbak.co.uk>
2014-06-02 15:15:09 +0100

    Enlarge live area
    Remove unused imports

--- main.py
+++ main.py
@@ -1,28 +1,13 @@
-import logging as log # TODO proper logger config
-
-import numpy as np
-
 import kivy
 kivy.require('1.8.0')
 
 from kivy.core.window import Window
 from kivy.app import App
-#from kivy.config import Config
-#from kivy.uix.widget import Widget
-#from kivy.uix.relativelayout import RelativeLayout
-from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 
 from kivy.properties import (
-    #NumericProperty,
     ObjectProperty,
-    #ListProperty,
-    #ReferenceListProperty,
-    #BooleanProperty,
 )
-
-
-from kivy_grid_cells import DrawableGrid
 
 
 class CustomLayout(BoxLayout):
@@ -34,9 +19,11 @@ class GameOfLifeApp(App):
     def on_start(self):
         # TODO can we calculate this in the kv file?
         def refresh_grid_position(*args):
+            return  # FIXME fix grid positioning
             self.root.grid.center = self.root.grid.parent.center[:]
 
         Window.bind(on_resize=refresh_grid_position)
+        self.root.grid.init_cells()
         refresh_grid_position()
 
 
